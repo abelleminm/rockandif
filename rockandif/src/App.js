fc00947d8e7563f5cb3f0735363bbe813f7db182
mayline0104 <mayline.le-polles@insa-lyon.fr>
2022-12-13 23:19:51 +0100

    Moroccan groups

--- rockandif/src/App.js
+++ rockandif/src/App.js
@@ -9,6 +9,7 @@ import Header from './Components/Header';
 import { Group } from '@mui/icons-material';
 import GroupCard from './Components/GroupCard';
 import FrenchGroupsPage from './Pages/FrenchGroupsPage';
+import MoroccanGroupsPage from './Pages/MoroccanGroupsPage';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="person" element={<PersonPage />} />
         <Route path="single" element={<SinglePage />} />
         <Route path="french" element={<FrenchGroupsPage />} />
+        <Route path="moroccan" element={<MoroccanGroupsPage />} />
       </Routes>
     </div>
   );
