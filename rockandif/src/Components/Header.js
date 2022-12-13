148c625794d0e12b746e1f0fb0a904b21ea732f6
mayline0104 <mayline.le-polles@insa-lyon.fr>
2022-12-13 10:00:14 +0100

    header

--- rockandif/src/Components/Header.js
+++ rockandif/src/Components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Header.css";
 import logo from "../Images/Logo.PNG";
 import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
 
 function Header() {
     return (
