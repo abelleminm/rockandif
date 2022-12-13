9e37187e535fa6d181e249b0537d5d380da35e07
mayline0104 <mayline.le-polles@insa-lyon.fr>
2022-12-13 17:37:10 +0100

    French groups page

--- rockandif/src/App.js
+++ rockandif/src/App.js
@@ -6,11 +6,14 @@ import HomePage from './Pages/HomePage';
 import PersonPage from './Pages/PersonPage';
 import SinglePage from './Pages/SinglePage';
 import Header from './Components/Header';
+import { Group } from '@mui/icons-material';
+import GroupCard from './Components/GroupCard';
+import FrenchGroupsPage from './Pages/FrenchGroupsPage';
 
 function App() {
   return (
     <div className="App">
-      <SinglePage />
+      <FrenchGroupsPage/>
     </div>
   );
 }
