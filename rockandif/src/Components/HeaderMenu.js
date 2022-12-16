aced65e1e908a62b53aa983c8ec2f82d7247e13e
t da <taddeo.d-adamo@insa-lyon.fr>
2022-12-16 11:06:40 +0100

    ajout de PageNotFound

---
+++ rockandif/src/Components/HeaderMenu.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./HeaderMenu.css";
+import MenuItem from "@mui/material/MenuItem";
+import { Fab} from "@mui/material";
+import Menu from "@mui/material/Menu";
+import { Link } from 'react-router-dom';
+
+
+function HeaderMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Fab id="boutonMenu" aria-label="add"
+            onClick={handleClick}>
+                Menu
+            </Fab>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'boutonMenu',
+                }}
+            >
+                <MenuItem onClick={handleClose}><Link to="/french-groups">French groups</Link></MenuItem>
+                <MenuItem onClick={handleClose}><Link to="/moroccan-groups">Maroccan groups</Link></MenuItem>
+                <MenuItem onClick={handleClose}>HELP (easter egg)</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+
+export default HeaderMenu;
