554930c85021ccf18ef31ad855ecbd2be9359a0b
GwenACL <gwenaelle.ancel@orange.fr>
2022-12-24 11:07:36 +0100

    combobox personnalisée

--- rockandif/src/Components/Combobox.js
+++ rockandif/src/Components/Combobox.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./Combobox.css";
 
 function Combobox() {
-    return (
-        <div id="combobox" class="custom-select">
-            <select>
-                <option value="band">Rock Band</option>
-                <option value="album">Album</option>
-                <option value="single">Single</option>
-                <option value="person">Member of the band</option>
-                <option value="date">Date of creation</option>
-            </select>
+    return (   
+        <div id="wrapper" class="myWrapper">
+            <label for="combobox">Filter : </label>
+            <div id="combobox" class="custom-select">
+                <select id="mySelect">
+                    <option value="band">Rock Band</option>
+                    <option value="album">Album</option>
+                    <option value="single">Single</option>
+                    <option value="person">Member of the band</option>
+                    <option value="date">Date of creation</option>
+                </select>
+                <span class="focus"></span>
+            </div>
         </div>
     );
 }
