35a832c484cc1429c8fa6cd28e285aaf43c3e4a1
tdadamo <taddeo.d-adamo@insa-lyon.fr>
2022-12-26 13:24:03 +0100

    correction faute d'orthographe menu

--- rockandif/src/Components/HeaderMenu.js
+++ rockandif/src/Components/HeaderMenu.js
@@ -32,7 +32,7 @@ function HeaderMenu() {
             >
                 <MenuItem onClick={handleClose}><Link to="/">Home</Link></MenuItem>
                 <MenuItem onClick={handleClose}><Link to="/french-groups">French groups</Link></MenuItem>
-                <MenuItem onClick={handleClose}><Link to="/moroccan-groups">Maroccan groups</Link></MenuItem>
+                <MenuItem onClick={handleClose}><Link to="/moroccan-groups">Moroccan groups</Link></MenuItem>
                 <MenuItem onClick={handleClose}>HELP (easter egg)</MenuItem>
             </Menu>
         </div>
