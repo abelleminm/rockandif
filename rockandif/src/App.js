74e977c3be6418da3735b3b48e684a256bb86786
ClementINSALYON <clement.bousseau@insa-lyon.fr>
2022-12-29 11:55:16 +0100

    created help page

--- rockandif/src/App.js
+++ rockandif/src/App.js
@@ -9,6 +9,7 @@ import MoroccanGroupsPage from './Pages/MoroccanGroupsPage';
 import { Routes, Route } from 'react-router-dom';
 import SearchResultsPage from './Pages/SearchResultsPage';
 import PageNotFound from './Pages/PageNotFound';
+import HelpPage from './Pages/HelpPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="person/:nom" element={<PersonPage/>} />
         <Route path="single/:band/:album/:title" element={<SinglePage/>} />
         <Route path="french-groups" element={<FrenchGroupsPage />} />
+        <Route path="help" element={<HelpPage />} />
         <Route path="moroccan-groups" element={<MoroccanGroupsPage />} />
         <Route path="search/:type/:text/:number" element={<SearchResultsPage />} />
         <Route path="*" element={<PageNotFound />} />
--- rockandif/src/Components/HeaderMenu.js
+++ rockandif/src/Components/HeaderMenu.js
@@ -33,7 +33,7 @@ function HeaderMenu() {
                 <MenuItem onClick={handleClose}><Link to="/">Home</Link></MenuItem>
                 <MenuItem onClick={handleClose}><Link to="/french-groups">French groups</Link></MenuItem>
                 <MenuItem onClick={handleClose}><Link to="/moroccan-groups">Moroccan groups</Link></MenuItem>
-                <MenuItem onClick={handleClose}>HELP (easter egg)</MenuItem>
+                <MenuItem onClick={handleClose}><Link to="/help">Help</Link></MenuItem>
             </Menu>
         </div>
     );
